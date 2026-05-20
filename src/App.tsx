cc3432d33f45170323cd2b8fcff41e1d32e9d85f
Gala82Sh <galchonok_gala@mail.ru>
2026-05-20 19:51:51 +0300

    настройка проекта SkyFitnessPro

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1 style={{ color: 'var(--primary)' }}>SkyFitnessPro</h1>
+      <p>Проект готов к работе!</p>
+    </div>
+  )
+}
+
+export default App
